6adece4aef109679b664bf2b066d4e1ff833731e
Georgina Yeboah <gina.yo@hotmail.com>
2020-04-02 16:28:39 -0400

    Link in different location.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -212,6 +212,9 @@ export default class App extends React.Component<{}> {
   }
 
   drawInstructions = (p5: P5) => {
+
+    const link = p5.createA('https://air-and-water.format.com/', 'Back to Air + Water site')
+    link.position(100,50)
     p5.push()
     p5.textSize(16)
     p5.fill(130)
@@ -220,9 +223,6 @@ export default class App extends React.Component<{}> {
     p5.text('Press J, K or L to make some bubbles!', 200, 130)
     p5.text('The icons represent user count. If your not alone in isolation, get others infront of the camera to access more colour sets!', 200,160)
     p5.text("If you'd like, feel free to take a screenshot and send it at georgina.yeboah@student.ocadu.ca. I'll post it to the site afterwards!", 200,190)
-
-    const link = p5.createA('https://air-and-water.format.com/', 'Back to Air + Water site')
-    link.position(100,50)
     p5.pop()
   }
 
